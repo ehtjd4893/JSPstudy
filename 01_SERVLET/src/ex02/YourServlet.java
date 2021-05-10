f2139368c5a1b4efdcd28686c2f2bad7dc07e9ba
ehtjd4893 <nsop4893@gmail.com>
2021-05-10 12:33:31 +0900

    2021-05-10 12:33 update

---
+++ 01_SERVLET/src/ex02/YourServlet.java
@@ -0,0 +1,44 @@
+package ex02;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class YourServlet
+ * 1. 서블릿을 실행하는 방법
+ * 	1) 톰캣을 실행한다.
+ * 	2) 
+ */
+@WebServlet("/galaxy")
+public class YourServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public YourServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
